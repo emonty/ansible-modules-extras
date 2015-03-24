b65e4f770b3941529cbd448581533faa6664d736
David Shrewsbury <shrewsbury.dave@gmail.com>
2015-03-24 19:38:40 +0000

    Update os_router to use latest shade
    Remove dependency on neutron client and use shade library only.

--- cloud/os_router.py
+++ cloud/os_router.py
@@ -42,7 +42,7 @@ options:
      required: true
    admin_state_up:
      description:
-        - desired admin state of the created router .
+        - Desired admin state of the created router.
      required: false
      default: true
 requirements: ["shade"]
@@ -50,75 +50,50 @@ requirements: ["shade"]
 
 EXAMPLES = '''
 # Creates a router for tenant admin
-- os_router: state=present
-             username=admin
-             password=admin
-             project_name=admin
-             name=router1"
+- os_router:
+    state=present
+    name=router1
+    admin_state_up=True
 '''
 
-def _get_router_id(module, neutron):
-    kwargs = {
-            'name': module.params['name'],
-    }
-    try:
-        routers = neutron.list_routers(**kwargs)
-    except Exception, e:
-        module.fail_json(msg = "Error in getting the router list: %s " % e.message)
-    if not routers['routers']:
-        return None
-    return routers['routers'][0]['id']
-
-def _create_router(module, neutron):
-    router = {
-            'name': module.params['name'],
-            'admin_state_up': module.params['admin_state_up'],
-    }
-    try:
-        new_router = neutron.create_router(dict(router=router))
-    except Exception, e:
-        module.fail_json( msg = "Error in creating router: %s" % e.message)
-    return new_router['router']['id']
-
-def _delete_router(module, neutron, router_id):
-    try:
-        neutron.delete_router(router_id)
-    except:
-        module.fail_json("Error in deleting the router")
-    return True
 
 def main():
     argument_spec = openstack_full_argument_spec(
-        name                            = dict(required=True),
-        state                           = dict(default='present', choices=['absent', 'present']),
-        admin_state_up                  = dict(type='bool', default=True),
+        name=dict(required=True),
+        admin_state_up=dict(type='bool', default=True),
+        state=dict(default='present', choices=['absent', 'present']),
     )
+
     module_kwargs = openstack_module_kwargs()
     module = AnsibleModule(argument_spec, **module_kwargs)
 
     if not HAS_SHADE:
         module.fail_json(msg='shade is required for this module')
 
+    name = module.params['name']
+    admin_state_up = module.params['admin_state_up']
+    state = module.params['state']
+
     try:
         cloud = shade.openstack_cloud(**module.params)
-        neutron = cloud.neutron_client
+        router = cloud.get_router(name)
 
-
-        if module.params['state'] == 'present':
-            router_id = _get_router_id(module, neutron)
-            if not router_id:
-                router_id = _create_router(module, neutron)
-                module.exit_json(changed=True, result="Created", id=router_id)
+        if state == 'present':
+            if not router:
+                router = cloud.create_router(name, admin_state_up)
+                module.exit_json(changed=True, result="Created",
+                                 id=router['id'])
             else:
-                module.exit_json(changed=False, result="success" , id=router_id)
+                module.exit_json(changed=False, result="Success",
+                                 id=router['id'])
 
-        else:
-            router_id = _get_router_id(module, neutron)
-            if not router_id:
-                module.exit_json(changed=False, result="success")
+        elif state == 'absent':
+            if not router:
+                module.exit_json(changed=False, result="Success")
             else:
-                _delete_router(module, neutron, router_id)
-                module.exit_json(changed=True, result="deleted")
+                cloud.delete_router(name)
+                module.exit_json(changed=True, result="Deleted")
+
     except shade.OpenStackCloudException as e:
         module.fail_json(msg=e.message)
 
