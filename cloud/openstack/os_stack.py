83ae06b1739309f1dd1e494985a0c0504b56294d
Mathieu Bultel <mbultel@redhat.com>
2016-04-18 12:20:22 +0200

    Add copyright for os_stack module

--- cloud/openstack/os_stack.py
+++ cloud/openstack/os_stack.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python
 #coding: utf-8 -*-
 
+# (c) 2016, Mathieu Bultel <mbultel@redhat.com>
+# (c) 2016, Steve Baker <sbaker@redhat.com>
+#
+# This module is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this software.  If not, see <http://www.gnu.org/licenses/>.
+
 from time import sleep
 try:
     import shade
